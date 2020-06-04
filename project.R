b8d8e803b3afd4ec4c891d9d15191e00caccef93
JihanYu <styjh80@gmail.com>
2020-06-04 18:00:49 +0900

    modify project

--- project.R
+++ project.R
@@ -9,31 +9,16 @@ y_train <- read.table("./data/train/Y_train.txt", colClasses="character")
 subject_train <- read.table("./data/train/subject_train.txt", colClasses="character")
 train_data <- cbind(subject_train, y_train, x_train)
 
-body_acc_x_train <- read.table("./data/train/Inertial Signals/body_acc_x_train.txt", colClasses="character")
-body_acc_y_train <- read.table("./data/train/Inertial Signals/body_acc_y_train.txt", colClasses="character")
-body_acc_z_train <- read.table("./data/train/Inertial Signals/body_acc_z_train.txt", colClasses="character")
-body_gyro_x_train <- read.table("./data/train/Inertial Signals/body_gyro_x_train.txt", colClasses="character")
-body_gyro_y_train <- read.table("./data/train/Inertial Signals/body_gyro_y_train.txt", colClasses="character")
-body_gyro_z_train <- read.table("./data/train/Inertial Signals/body_gyro_z_train.txt", colClasses="character")
-total_acc_x_train <- read.table("./data/train/Inertial Signals/total_acc_x_train.txt", colClasses="character")
-total_acc_y_train <- read.table("./data/train/Inertial Signals/total_acc_y_train.txt", colClasses="character")
-total_acc_z_train <- read.table("./data/train/Inertial Signals/total_acc_z_train.txt", colClasses="character")
-
-
 x_test <- read.table("./data/test/X_test.txt", colClasses="character")
 y_test <- read.table("./data/test/Y_test.txt", colClasses="character")
 subject_test <- read.table("./data/test/subject_test.txt", colClasses="character")
 test_data <- cbind(subject_test, y_test, x_test)
 
-body_acc_x_test <- read.table("./data/test/Inertial Signals/body_acc_x_test.txt", colClasses="character")
-body_acc_y_test <- read.table("./data/test/Inertial Signals/body_acc_y_test.txt", colClasses="character")
-body_acc_z_test <- read.table("./data/test/Inertial Signals/body_acc_z_test.txt", colClasses="character")
-body_gyro_x_test <- read.table("./data/test/Inertial Signals/body_gyro_x_test.txt", colClasses="character")
-body_gyro_y_test <- read.table("./data/test/Inertial Signals/body_gyro_y_test.txt", colClasses="character")
-body_gyro_z_test <- read.table("./data/test/Inertial Signals/body_gyro_z_test.txt", colClasses="character")
-total_acc_x_test <- read.table("./data/test/Inertial Signals/total_acc_x_test.txt", colClasses="character")
-total_acc_y_test <- read.table("./data/test/Inertial Signals/total_acc_y_test.txt", colClasses="character")
-total_acc_z_test <- read.table("./data/test/Inertial Signals/total_acc_z_test.txt", colClasses="character")
+train.data <- cbind(subject_train, y_train, x_train)
+test.data <- cbind(subject_test, y_test, x_test)
+
+total.data <- rbind(train.data, test.data)
+total.data <- total.data[order(total.data[,1], total.data[,2]),]
 
 
 features <- read.table("./data/features.txt", colClasses="character")
@@ -55,9 +40,27 @@ names(df.features) <- c("num", "t.f", "name", "variable", "axis")
 mean.id <- which(df.features$variable=="mean()")
 std.id <- which(df.features$variable=="std()")
 
-train.data <- cbind(subject_train, y_train, x_train)
-test.data <- cbind(subject_test, y_test, x_test)
+meanstd.id <- sort(c(mean.id, std.id))
+meanstd.data <- total.data[, c(1, 2, meanstd.id+2)]
 
-total.data <- rbind(train.data, test.data)
-total.data <- total.data[order(total.data[,1], total.data[,2]),]
 
+
+body_acc_x_train <- read.table("./data/train/Inertial Signals/body_acc_x_train.txt", colClasses="character")
+body_acc_y_train <- read.table("./data/train/Inertial Signals/body_acc_y_train.txt", colClasses="character")
+body_acc_z_train <- read.table("./data/train/Inertial Signals/body_acc_z_train.txt", colClasses="character")
+body_gyro_x_train <- read.table("./data/train/Inertial Signals/body_gyro_x_train.txt", colClasses="character")
+body_gyro_y_train <- read.table("./data/train/Inertial Signals/body_gyro_y_train.txt", colClasses="character")
+body_gyro_z_train <- read.table("./data/train/Inertial Signals/body_gyro_z_train.txt", colClasses="character")
+total_acc_x_train <- read.table("./data/train/Inertial Signals/total_acc_x_train.txt", colClasses="character")
+total_acc_y_train <- read.table("./data/train/Inertial Signals/total_acc_y_train.txt", colClasses="character")
+total_acc_z_train <- read.table("./data/train/Inertial Signals/total_acc_z_train.txt", colClasses="character")
+
+body_acc_x_test <- read.table("./data/test/Inertial Signals/body_acc_x_test.txt", colClasses="character")
+body_acc_y_test <- read.table("./data/test/Inertial Signals/body_acc_y_test.txt", colClasses="character")
+body_acc_z_test <- read.table("./data/test/Inertial Signals/body_acc_z_test.txt", colClasses="character")
+body_gyro_x_test <- read.table("./data/test/Inertial Signals/body_gyro_x_test.txt", colClasses="character")
+body_gyro_y_test <- read.table("./data/test/Inertial Signals/body_gyro_y_test.txt", colClasses="character")
+body_gyro_z_test <- read.table("./data/test/Inertial Signals/body_gyro_z_test.txt", colClasses="character")
+total_acc_x_test <- read.table("./data/test/Inertial Signals/total_acc_x_test.txt", colClasses="character")
+total_acc_y_test <- read.table("./data/test/Inertial Signals/total_acc_y_test.txt", colClasses="character")
+total_acc_z_test <- read.table("./data/test/Inertial Signals/total_acc_z_test.txt", colClasses="character")
